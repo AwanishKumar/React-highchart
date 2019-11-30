ecb27eaa359a7d61cbcb5fc26ad7fb74a42837c6
Awanish Kumar <awanish.kumar@verantos.com>
2019-11-30 19:19:21 +0530

    Included highcharts

---
+++ build/precache-manifest.716a9c606a5270652243a67c66b330d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab1243c11d9bc7d89dc3a96643b634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b782a5e44ffe0857e2f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "63e0766ae605e269a5c5",
+    "url": "/static/js/2.14724b04.chunk.js"
+  },
+  {
+    "revision": "9b782a5e44ffe0857e2f",
+    "url": "/static/js/main.866883a5.chunk.js"
+  },
+  {
+    "revision": "989602fb21984efc988c",
+    "url": "/static/js/runtime-main.0eaeebce.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
